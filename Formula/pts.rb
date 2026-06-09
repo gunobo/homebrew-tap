5cd3b7ccb73e8282d37cff98a392f518a1148c0e
임제민 <startea0716@naver.com>
2026-06-09 17:53:31 +0900

    pts: v1.0.0

--- Formula/pts.rb
+++ Formula/pts.rb
@@ -2,7 +2,7 @@ class Pts < Formula
   desc "macOS 포트 사용 현황 확인 CLI 도구"
   homepage "https://github.com/gunobo/PortSearcher"
   url "https://github.com/gunobo/PortSearcher/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "PLACEHOLDER_SHA256"
+  sha256 "cbd17807c3582fa28ac8fef796cf92f547903c89043c08bda95782257c08d971"
   license "MIT"
 
   depends_on :macos
