a624a2e8e14e74333e8fdb9826b64dca6af77839
임제민 <startea0716@naver.com>
2026-06-09 18:07:14 +0900

    fix: 바이너리 URL을 homebrew-tap 릴리즈로 변경

--- Formula/pts.rb
+++ Formula/pts.rb
@@ -1,7 +1,7 @@
 class Pts < Formula
   desc "macOS 포트 사용 현황 확인 CLI 도구"
   homepage "https://github.com/gunobo/PortSearcher"
-  url "https://github.com/gunobo/PortSearcher/releases/download/v1.0.0/pts-arm64.tar.gz"
+  url "https://github.com/gunobo/homebrew-tap/releases/download/v1.0.0/pts-arm64.tar.gz"
   sha256 "ce639ea7716c9c369dcea79b61af8ee494502a30d7514b4c23b6961cba59a6ed"
   license "MIT"
   version "1.0.0"
