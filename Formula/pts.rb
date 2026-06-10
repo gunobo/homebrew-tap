85a2221579f9be5a3dfe7e3dbd29fadc055a1924
임제민 <startea0716@naver.com>
2026-06-10 11:05:23 +0900

    update pts to v1.2.0

--- Formula/pts.rb
+++ Formula/pts.rb
@@ -1,10 +1,10 @@
 class Pts < Formula
   desc "macOS 포트 사용 현황 확인 CLI 도구"
   homepage "https://github.com/gunobo/PortSearcher"
-  url "https://github.com/gunobo/homebrew-tap/releases/download/v1.0.0/pts-arm64.tar.gz"
-  sha256 "a23c9d1d34737629fe6145798ae5a0377b3d15f795124c34257253ad33d7c499"
+  url "https://github.com/gunobo/PortSearcher/releases/download/v1.2.0/pts-arm64.tar.gz"
+  sha256 "2be19aa67570e49409af6a0033e8fa9cc68e2856e404a0b3f220ee6f88f86aa3"
   license "MIT"
-  version "1.0.0"
+  version "1.2.0"
 
   depends_on :macos
 
